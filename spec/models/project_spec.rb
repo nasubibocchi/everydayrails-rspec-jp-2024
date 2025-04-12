b2b8f2cca49de1f84c6d1eccf6fdc1b9292ac418
nasubibocchi <matsuhito125@gmail.com>
2025-04-12 18:09:05 +0900

    演習問題

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  describe "validations" do
+    before do
+      @user = User.create(
+        first_name: "John",
+        last_name: "Tester",
+        email: "johntester@example.com",
+        password: "dottle-nouveau-pavilion-tights-furze",
+      )
+    end
+
+    it "is valid with name and user" do
+      project = Project.new(
+        name: "Test Project",
+        owner: @user,
+      )
+      expect(project).to be_valid
+    end
+
+    it "is invalid without a name" do
+      project = Project.new(
+        name: nil,
+        owner: @user,
+      )
+      project.valid?
+      expect(project.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid without an owner" do
+      project = Project.new(
+        name: "Test Project",
+        owner: nil,
+      )
+      project.valid?
+      expect(project.errors[:owner]).to include("must exist")
+    end
+  end
+
   it "does not allow duplicate project names for the same user" do
     user = User.create(
       first_name: "John",
