36a87875cfce75889337da489ba77c78f5ac151a
nasubibocchi <matsuhito125@gmail.com>
2025-06-01 14:31:27 +0900

    Build test data using trait

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -49,17 +49,17 @@ RSpec.describe Project, type: :model do
 
   describe "date status" do
     it 'is late when due date is in the past today' do
-      project = FactoryBot.create(:project_due_yesterday)
+      project = FactoryBot.create(:project, :due_yesterday)
       expect(project).to be_late
     end
 
     it 'is on time when the due date is today' do
-      project = FactoryBot.create(:project_due_today)
+      project = FactoryBot.create(:project, :due_today)
       expect(project).not_to be_late
     end
 
     it 'is on time when the due date is in the future' do
-      project = FactoryBot.create(:project_due_tomorrow)
+      project = FactoryBot.create(:project, :due_tomorrow)
       expect(project).not_to be_late
     end
   end
