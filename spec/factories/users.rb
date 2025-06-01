b7a6c4c78da949f384f5f6d96edd0efbbd550a0c
nasubibocchi <matsuhito125@gmail.com>
2025-06-01 15:17:14 +0900

    Exercises Chapter 3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :user, aliases: [:owner] do
-    first_name { "Aaron" }
-    last_name { "Summer" }
+    first_name { "Tester" }
+    last_name { "Example" }
     sequence(:email) { |n| "tester#{n}@example.com" }
     password { "dottle-nouveau-pavilion-tights-furze" }
+
+    trait :with_a_project do
+      after(:create) { |user| create(:project, owner: user, name: 'Test Project') }
+    end
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,47 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-      )
-
-    user.projects.create(
-      name: "Test Project",
-      )
-
-    new_project = user.projects.build(
-      name: "Test Project",
-      )
+    user = FactoryBot.create(:user, :with_a_project)
+    project = user.projects.first
+    new_project = user.projects.build(name: project.name)
 
     new_project.valid?
     expect(new_project.errors[:name]).to include("has already been taken")
   end
 
   it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-      )
-
-    user.projects.create(
-      name: "Test Project",
-      )
-
-    other_user = User.create(
-      first_name: "Jane",
-      last_name:  "Tester",
-      email:      "janetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-      )
-
-    other_project = other_user.projects.build(
-      name: "Test Project",
-      )
+    user = FactoryBot.create(:user, :with_a_project)
+    other_user = FactoryBot.create(:user)
+    other_project = other_user.projects.build(name: user.projects.first.name)
 
     expect(other_project).to be_valid
   end
