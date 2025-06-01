66349d2e47ac3801e4d8486e27ea523876189a9b
nasubibocchi <matsuhito125@gmail.com>
2025-06-01 14:23:15 +0900

    Use inheritance to remove duplicate code in factories

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,27 +4,17 @@ FactoryBot.define do
     description { "A test project." }
     due_on { 1.week.from_now }
     association :owner
-  end
 
-  factory :project_due_yesterday, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description { "Sample project for testing purposes" }
-    due_on { 1.day.ago }
-    association :owner
-  end
+    factory :project_due_yesterday, class: Project do
+      due_on { 1.day.ago }
+    end
 
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description { "Sample project for testing purposes" }
-    due_on { Date.current.in_time_zone }
+    factory :project_due_today, class: Project do
+      due_on { Date.current.in_time_zone }
+    end
 
-    association :owner
-  end
-
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description { "Sample project for testing purposes" }
-    due_on { 1.day.from_now }
-    association :owner
+    factory :project_due_tomorrow, class: Project do
+      due_on { 1.day.from_now }
+    end
   end
 end
