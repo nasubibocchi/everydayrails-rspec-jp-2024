0884fa30f41728baaa6a86c5035d16a6c72600e7
nasubibocchi <matsuhito125@gmail.com>
2025-06-01 14:28:58 +0900

    Remove unneeded class options

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -5,15 +5,15 @@ FactoryBot.define do
     due_on { 1.week.from_now }
     association :owner
 
-    factory :project_due_yesterday, class: Project do
+    factory :project_due_yesterday do
       due_on { 1.day.ago }
     end
 
-    factory :project_due_today, class: Project do
+    factory :project_due_today do
       due_on { Date.current.in_time_zone }
     end
 
-    factory :project_due_tomorrow, class: Project do
+    factory :project_due_tomorrow do
       due_on { 1.day.from_now }
     end
   end
