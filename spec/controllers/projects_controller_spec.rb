f4e9b8c5d3b743a680f007018fea639c9071ec55
nasubibocchi <matsuhito125@gmail.com>
2025-07-26 15:39:12 +0900

    before_actionが機能しているかのテスト

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -32,4 +32,33 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+
+  describe '#show' do
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      it "responds successfully" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to be_successful
+      end
+    end
+
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      it "redirects to the root path" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
