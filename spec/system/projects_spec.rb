6c07eddc06bbea6060c99c4bd13560dae142ce06
nasubibocchi <matsuhito125@gmail.com>
2025-11-01 19:40:28 +0900

    サポートモジュール: sign_in

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe "Projects", type: :system do
   scenario "user creates a new project" do
     user = FactoryBot.create(:user)
-
+    
+    ## LoginSupport module
+    # sign_in_as user
+    
+    # Devise
+    sign_in user
     visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
 
     expect {
       click_link "New Project"
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -8,11 +8,12 @@ RSpec.describe "Tasks", type: :system do
                                 owner: user)
     task = project.tasks.create!(name: "Finish RSpec tutorial")
 
+    # # LoginSupport module
+    # sign_in_as user
+
+    # Devise
+    sign_in user
     visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
 
     click_link "RSpec tutorial"
     check "Finish RSpec tutorial"
